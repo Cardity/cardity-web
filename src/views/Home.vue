08660d0dc043c0b43a755c3cf2905206a8df51ce
Peter Lohse <peterlohse@yahoo.de>
2021-03-04 12:33:36 +0100

    typescript fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,51 @@
 <template>
-  <div class="home">
-    Home
-  </div>
+    <div class="home">
+        <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field
+                v-model="name"
+                :counter="10"
+                :rules="nameRules"
+                label="Name"
+                required
+            ></v-text-field>
+
+            <v-btn
+                :disabled="!valid"
+                depressed
+                @click="validate"
+            >
+                Submit
+            </v-btn>
+        </v-form>
+    </div>
 </template>
+
+<script lang="ts">
+import { Component, Ref, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Home extends Vue {
+    $refs!: {
+        form: HTMLFormElement
+    }
+
+    data() {
+        return {
+            valid: true,
+            name: '',
+            nameRules: [
+                (value: string) => !!value || "Test",
+                (value: string) => (value && value.length > 10) || "Name muss größer als 10 Zeichen sein"
+            ]
+        };
+    }
+
+    validate() {
+        if (!this.$refs.form.validate()) {
+            return;
+        }
+
+        console.log(this.$refs.form);
+    }
+}
+</script>
