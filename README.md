9fecee71ed1fea59dd295158b241319158ff3142
Peter Lohse <peterlohse@yahoo.de>
2021-03-04 08:31:42 +0100

    cleanup

--- README.md
+++
@@ -1,4 +0,0 @@
-# cah_web
-
-npm install -g sass
-sass [--watch] scss/cah.scss src/css/cah.css
--- ts/cah.ts
+++
@@ -1,5 +0,0 @@
-import { Client } from "./client/client";
-
-let client = new Client({
-    "websocketPath": "{WEBSOCKET_PATH}"
-});
